c92e0f4e6d94dac2d50e4f64086c2b67e98ee5e3
tplamondon <tplamondon@tutanota.de>
2021-01-27 20:25:03 -0800

    switched to embeds, fixed exception issues

--- source/source.py
+++ source/source.py
@@ -1,6 +1,11 @@
 from redbot.core import commands
 import tracemoepy
+from tracemoepy.errors import TooManyRequests
+from tracemoepy.errors import EntityTooLarge
+from tracemoepy.errors import ServerError
+from tracemoepy.errors import EmptyImage
 import math
+from discord import Embed
 
 class Source(commands.Cog):
     """Looks up source of anime and pictures, may get NSFW results. Requires tracemoepy to be installed"""
@@ -23,11 +28,18 @@ class Source(commands.Cog):
             anilistID = f'{result.docs[0].anilist_id}'
             episode = f'{result.docs[0].episode}'
             similarity = float(f'{result.docs[0].similarity}')
-            url = "https://anilist.co/anime/"+anilistID
+            URL = "https://anilist.co/anime/"+anilistID
+
+            embed = Embed(title=titleEnglish, url=URL)
+            embed.add_field(name="Episode", value=episode)
+            embed.add_field(name="Similarity", value=('%.3f'%((similarity)*100)))
             if(similarity < 0.8):
-                await ctx.send("Anime: " + titleEnglish + "\nEpisode: " + episode +"\nWARNING: Similarity less than 80%, result may not be accurate"+ "\n"+url)
+                #await ctx.send("Anime: " + titleEnglish + "\nEpisode: " + episode +"\nWARNING: Similarity less than 80%, result may not be accurate"+ "\n"+URL)
+                embed.add_field(name="Warning", value="Similarity less than  80%, results may be innacurate")
+                await ctx.send(embed=embed)
             else:
-                await ctx.send("Anime: " + titleEnglish + "\nSimilarity: " + ('%.3f'%((similarity)*100)) +"%" + "\nEpisode: " + episode + "\n"+url)
+                #await ctx.send("Anime: " + titleEnglish + "\nSimilarity: " + ('%.3f'%((similarity)*100)) +"%" + "\nEpisode: " + episode + "\n"+URL)
+                await ctx.send(embed=embed)
         except TooManyRequests:
             await ctx.send("Too many requests sent")
         except EntityTooLarge:
