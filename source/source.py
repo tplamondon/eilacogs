bcdd48f8051618ab062c5b8b9e2ce15ffc7797af
tplamondon <tplamondon@tutanota.de>
2021-01-27 21:27:31 -0800

    let you upload images to search on source

--- source/source.py
+++ source/source.py
@@ -52,3 +52,53 @@ class Source(commands.Cog):
             await ctx.send("Empty image provided. Ensure image is provided as URL and points directly to png or jpg image")
         #except InvalidPath:
         #    await ctx.send("Invalid path, bot had an error with .save method")
+
+    @commands.command()
+    async def source(self, ctx):
+        """
+        Looks for source of image
+
+        Parameters:
+        -----------
+        make sure to attach a png or jpg image, or just provide a URL
+        """
+
+        helpmsg = "Looks for source of image\nMake sure to attach a png or jpg image or provide a direct URL to one"
+        tracemoe = tracemoepy.tracemoe.TraceMoe()
+        try:
+            await ctx.trigger_typing()
+            try:
+                attachment = ctx.message.attachments[0].url
+            except:
+                await ctx.send(helpmsg)
+                return
+
+            result = tracemoe.search(attachment.strip("<>"), is_url = True)
+            titleEnglish = f'{result.docs[0].title_english}'
+            anilistID = f'{result.docs[0].anilist_id}'
+            episode = f'{result.docs[0].episode}'
+            similarity = float(f'{result.docs[0].similarity}')
+            URL = "https://anilist.co/anime/"+anilistID
+
+            #embed = Embed(title=titleEnglish, url=URL)
+            #embed.add_field(name="Episode", value=episode)
+            #embed.add_field(name="Similarity", value=('%.3f'%((similarity)*100)))
+            if(similarity < 0.8):
+                await ctx.send("Anime: " + titleEnglish + "\nEpisode: " + episode +"\nWARNING: Similarity less than 80%, result may not be accurate"+ "\n"+URL)
+                #embed.add_field(name="Warning", value="Similarity less than  80%, results may be innacurate")
+                await ctx.send(embed=embed)
+            else:
+                await ctx.send("Anime: " + titleEnglish + "\nSimilarity: " + ('%.3f'%((similarity)*100)) +"%" + "\nEpisode: " + episode + "\n"+URL)
+                #await ctx.send(embed=embed)
+        except TooManyRequests:
+            await ctx.send("Too many requests sent")
+        except EntityTooLarge:
+            await ctx.send("Too big of file image")
+        except ServerError:
+            await ctx.send("Server error. Ensure image is provided as URL and points directly to png or jpg image")
+        #except InvalidToken:
+        #    await ctx.send("Invalid token")
+        except EmptyImage:
+            await ctx.send("Empty image provided. Ensure image is provided as URL and points directly to png or jpg image")
+        #except InvalidPath:
+        #    await ctx.send("Invalid path, bot had an error with .save method")
