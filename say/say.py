bfe0bf121c240f517428d98ee111dd5932887e0c
tplamondon <tplamondon@tutanota.de>
2021-01-27 17:39:41 -0800

    updated permissions on say cog

--- say/say.py
+++ say/say.py
@@ -1,10 +1,11 @@
 from redbot.core import commands
-
+from discord.ext.commands import has_permissions
 
 class Say(commands.Cog):
     """Makes the bot say something"""
 
     @commands.command()
+    @has_permissions(administrator=True)
     async def say(self, ctx, *message):
         """Make the bot say something"""
         str = " ".join(message)
