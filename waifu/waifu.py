646d91ba63b55eb555ca7e7b606573d10db92bc1
LPlamondon <LPlamondon@users.noreply.github.com>
2021-06-08 12:49:34 -0700

    Changed call to waifuAbout, and moved getImage out of class
    Instead of waifuAbout(ctx), I changed it to await self.waifuAbout(ctx)

--- waifu/waifu.py
+++ waifu/waifu.py
@@ -67,7 +67,7 @@ class Waifu(commands.Cog):
         """Display a random waifu"""
         if not imageType:
             # Display about this module if no command passed
-            waifuAbout(ctx)
+            await self.waifuAbout(ctx)
             return
         elif imageType.lower() not in IMAGE_CATEGORIES:
             imageType = "waifu"
@@ -93,23 +93,23 @@ class Waifu(commands.Cog):
         embed.set_footer(text="cogs/waifu")
         await ctx.send(embed=embed)
 
-    def getImage(image, title):
-        """
-        Take a passed url from Waifu.pics, and construct a discord.Embed object
 
-        Parameters:
-        -----------
-        image : a URL
-        title: a string
+def getImage(image, title):
+    """
+    Take a passed url from Waifu.pics, and construct a discord.Embed object
+    Parameters:
+    -----------
+    image : a URL
+    title: a string
 
-        Returns:
-        -----------
-        embed : discord.embed
-            a fully constructed discord.Embed object, ready to be sent as a message.
-        """
-        embed = discord.Embed()
-        embed.colour = discord.Colour.red()
-        embed.title = title
-        embed.url = image
-        embed.set_image(url=image)
-        return embed
+    Returns:
+    -----------
+    embed : discord.embed
+        a fully constructed discord.Embed object, ready to be sent as a message.
+    """
+    embed = discord.Embed()
+    embed.colour = discord.Colour.red()
+    embed.title = title
+    embed.url = image
+    embed.set_image(url=image)
+    return embed
