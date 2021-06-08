04c84d227ad750db689cfe8f11829988d2227c77
LPlamondon <LPlamondon@users.noreply.github.com>
2021-06-08 10:56:53 -0700

    Added global variables and import of Catgirl cog. Function for posting picture started

--- waifu/waifu.py
+++ waifu/waifu.py
@@ -4,9 +4,16 @@ import discord
 import requests
 from redbot.core import checks, Config, commands
 from redbot.core.bot import Red
+from .catgirl import Catgirl #For access to the getImageURL function
+
+#Global variables
+URL = "https://api.waifu.pics/sfw/"
 
 class Waifu(commands.cog):
     """Display waifu.pic pictures"""
 
     def __init__(self, bot: Red):
         self.bot = bot
+
+    def waifuCmd(self, ctx):
+        await ctx.channel.trigger_typing()
