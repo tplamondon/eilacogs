738c115b21c39e1525d7ebd3d6fc886294d4c624
LPlamondon <LPlamondon@users.noreply.github.com>
2021-06-08 11:32:48 -0700

    Added global variable to hold all valid arguments, and added waifuAbout
    waifuAbout is to display information about the module in the case no argument is passed to _waifu

--- waifu/waifu.py
+++ waifu/waifu.py
@@ -7,6 +7,10 @@ from redbot.core.bot import Red
 
 #Global variables
 URL = "https://api.waifu.pics/sfw/"
+IMAGE_CATEGORIES = [ "waifu", "neko", "shinobu", "megumin", "bully", "cuddle", "cry",
+"hug", "awoo", "kiss", "lick", "pat", "smug", "bonk", "yeet", "blush", "smile", "wave",
+"highfive", "handhold", "nom", "bite", "glomp", "slap", "kill", "happy", "wink", "poke",
+"dance", "cringe" ]
 
 class Waifu(commands.cog):
     """Display waifu.pic pictures"""
@@ -23,9 +27,31 @@ class Waifu(commands.cog):
     async def _waifu(self, ctx, imageType: str = None):
         """Display a random waifu"""
         if not imageType:
-            imageType = "waifu"
+            #Display about this module if no command passed
+            waifuAbout(ctx)
+            return
+
 
         await self.waifuCmd(ctx, imageType)
+
+    async def waifuAbout(self, ctx):
+        """Displays information about the waifu module"""
+        customAuthor = "@西木野 真姫#4354"
+        embed = discord.Embed()
+        embed.title = "About this module"
+        embed.add_field(name="Name", value="Waifu Module")
+        embed.add_field(name="Author", value=customAuthor)
+        embed.add_field(name="Initial Version Date", value="2021-06-08")
+        embed.add_field(
+            name="Description",
+            value="A module to display images from waifu.pics."
+            "Valid commands are as follows: waifu, neko, shinobu, megumin, bully"
+            "cuddle, cry, hug, awoo, kiss, lick, pat, smug, bonk, yeet, blush, smile"
+            "wave, highfive, handhold, nom, bite, glomp, slap, kill, happy, wink, poke"
+            "dance, cringe",
+        )
+        embed.set_footer(text="cogs/waifu")
+        await ctx.send(embed=embed)
 
 
     def getImage(image, title):
